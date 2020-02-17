849decb4ba71fdd48317e37a032e6115ba5da902
ahtashamalvi <61066687+ahtashamalvi@users.noreply.github.com>
2020-02-17 01:21:52 -0600

    Update places.js

--- places.js
+++ places.js
@@ -1,13 +1,13 @@
 const loadPlaces = function(coords) {
     // COMMENT FOLLOWING LINE IF YOU WANT TO USE STATIC DATA AND ADD COORDINATES IN THE FOLLOWING 'PLACES' ARRAY
-    //const method = 'api';
+    const method = 'api';
 
     const PLACES = [
         {
             name: "Your place name",
             location: {
-                lat: 38.9020, // add here latitude if using static data
-                lng: -94.7263, // add here longitude if using static data
+                lat: 0, // add here latitude if using static data
+                lng: 0, // add here longitude if using static data
 
             }
         },
